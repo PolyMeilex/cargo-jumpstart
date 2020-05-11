556d880971568f9f08ca06913d7aeccacfffcca7
Poly <marynczak.bartlomiej@gmail.com>
2020-05-11 20:33:26 +0200

    Update clap to work as cargo extenstion

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,19 @@ fn main() {
         .author("Poly <marynczak.bartlomiej@gmail.com>")
         .about("Util for rust file scaffolding")
         .subcommand(
-            App::new("object")
-                .about("Create rust file with object inside")
-                .arg(Arg::with_name("name")),
+            App::new("jumpstart").subcommand(
+                App::new("object")
+                    .about("Create rust file with object inside")
+                    .arg(Arg::with_name("name")),
+            ),
         )
         .get_matches();
 
-    if let Some(c) = matches.subcommand_matches("object") {
-        if let Some(o) = c.value_of("name") {
-            let file_str = format!(
-                r#"
+    if let Some(matches) = matches.subcommand_matches("jumpstart") {
+        if let Some(c) = matches.subcommand_matches("object") {
+            if let Some(o) = c.value_of("name") {
+                let file_str = format!(
+                    r#"
 pub struct {}{{
 }}
 
@@ -28,29 +31,30 @@ impl {}{{
     }}
 }}
                 "#,
-                o, o
-            );
+                    o, o
+                );
 
-            let chars: Vec<char> = o.chars().collect();
-            let mut words: Vec<Vec<char>> = Vec::new();
+                let chars: Vec<char> = o.chars().collect();
+                let mut words: Vec<Vec<char>> = Vec::new();
 
-            for c in chars {
-                if c.is_uppercase() {
-                    words.push(Vec::new());
+                for c in chars {
+                    if c.is_uppercase() {
+                        words.push(Vec::new());
+                    }
+
+                    let id = words.len() - 1;
+                    words[id].push(c);
                 }
 
-                let id = words.len() - 1;
-                words[id].push(c);
+                let words: Vec<String> = words
+                    .into_iter()
+                    .map(|w| w.into_iter().collect::<String>().to_lowercase())
+                    .collect();
+                let file_name = format!("{}.rs", words.join("_"));
+
+                let mut file = File::create(&file_name).unwrap();
+                file.write(file_str.as_bytes()).unwrap();
             }
-
-            let words: Vec<String> = words
-                .into_iter()
-                .map(|w| w.into_iter().collect::<String>().to_lowercase())
-                .collect();
-            let file_name = format!("{}.rs", words.join("_"));
-
-            let mut file = File::create(&file_name).unwrap();
-            file.write(file_str.as_bytes()).unwrap();
         }
     }
 }
